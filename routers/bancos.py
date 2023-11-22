493d800e30fee242d54f1d6c795c0e88874ce2ce
Alban Krug <alban.m.krug@gmail.com>
2023-11-22 16:26:52 -0300

    Editar banco funciona

--- routers/bancos.py
+++ routers/bancos.py
@@ -42,3 +42,31 @@ async def create_banco(db: Session = Depends(get_database_session), descripcion 
 async def listar_bancos(request: Request, db: Session = Depends(get_database_session)):
     bancos = db.query(Banco).all()
     return JSONResponse(jsonable_encoder(bancos))
+
+@router.get("/{id}",response_class=HTMLResponse)
+def ver(id:int, response:Response,
+             request:Request,db: Session = Depends(get_database_session)):
+     banc = db.query(Banco).get(id)
+     return templates.TemplateResponse("bancos/listar.html", {"request": request, "Banco": banc})
+
+@router.get("/editar/{id}",response_class=HTMLResponse)
+def editar_view(id:int,response:Response,request:Request,db: Session = Depends(get_database_session)):
+     banc= db.query(Banco).get(id)
+     return templates.TemplateResponse("bancos/editar.html", {"request": request, "Banco": banc})
+
+@router.post("/update",response_class=HTMLResponse)
+def editar(db: Session = Depends(get_database_session), idbanco = Form(...), descripcion = Form(...)):
+     banc= db.query(Banco).get(idbanco)
+     banc.descripcion=descripcion
+     db.add(banc)
+     db.commit()
+     db.refresh(banc)
+     response = RedirectResponse('/bancos/', status_code=303)
+     return response
+
+@router.get("/borrar/{id}",response_class=HTMLResponse)
+def eliminar(id : int, db: Session = Depends(get_database_session)):
+     db.query(Banco).filter(Banco.idbanco == id).delete()
+     db.commit()
+     response = RedirectResponse('/bancos/', status_code=303)
+     return response
