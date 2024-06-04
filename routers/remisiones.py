2ee8ef911d05267b0c8a33ef2a861a6dd9965a6b
Alban Krug <alban.m.krug@gmail.com>
2024-06-04 16:21:35 -0400

    backup

--- routers/remisiones.py
+++ routers/remisiones.py
@@ -12,7 +12,7 @@ from starlette import status
 from starlette.responses import RedirectResponse
 
 from db.misc import get_database_session
-from models import (Camion, Carreta, Chofer, Contrato, Producto, Deposito, Remision)
+from models import (Camion, Carreta, Chofer, Contrato, Producto, Deposito, Remision, Cliente)
 from routers import auth
 from schemas import usuario as us
 
@@ -61,15 +61,18 @@ async def create_remision(db: Session=Depends(get_database_session),usuario_actu
     response = RedirectResponse('/', status_code=303)
     return response
 
-# @router.get("/{id}",response_class=HTMLResponse)
-# def ver(id:int, response:Response,
-#             request:Request,db: Session = Depends(get_database_session), usuario_actual: us.Usuario = Depends(auth.get_usuario_actual)):
-#     remi = db.query(Remision).get(id)
-#     return templates.TemplateResponse("remisiones/listar.html", {"request": request, "Remision": remi, "usuario_actual": usuario_actual})
 
 @router.get("/ver/{id}")
 def ver(id:int, response:Response, request:Request,db: Session = Depends(get_database_session), usuario_actual: us.Usuario = Depends(auth.get_usuario_actual)):
-    remi= db.query(Remision.numero, Remision.fecha_carga, Remision.fecha_descarga, Remision.idcontrato).filter(Remision.idremision==id).first()
+    print(Request.body)
+    
+    remi= db.query(Remision.numero, Remision.fecha_carga, Remision.fecha_descarga, Remision.idcontrato, Cliente.descripcion.label('cliente'), Remision.idchofer, Remision.idcamion, Remision.idcarreta,
+                    Remision.bruto, Remision.tara, Remision.neto, Remision.brutod, Remision.tarad, Remision.netod).join(Contrato,Remision.idcontrato==Contrato.idcontrato).join(Cliente, Contrato.idcontrato==Cliente.idcliente).filter(Remision.idremision==id).first()
+
+    print(remi)
+    
+
+
     return templates.TemplateResponse("remisiones/ver.html", {"request": request, "usuario_actual": usuario_actual, "Remision":remi})
 
 
--- routers/reportes.py
+++ routers/reportes.py
@@ -65,12 +65,12 @@ def mostrar_stock(request: Request, db: Session = Depends(get_database_session),
     ).group_by(Factura_venta_detalle.idproducto, Factura_venta_cabecera.fecha)
 
     
-    query_remisiones = db.query(
-        literal('Remision').label('tipo_movimiento'),
-        Remision.idproducto,
-        func.sum(Remision.cantidad).label('cantidad'),
-        Remision.fecha.label('fecha')
-    ).group_by(Remision.idproducto, Remision.fecha)
+    # query_remisiones = db.query(
+    #     literal('Remision').label('tipo_movimiento'),
+    #     Remision.idproducto,
+    #     func.sum(Remision.cantidad).label('cantidad'),
+    #     Remision.fecha.label('fecha')
+    # ).group_by(Remision.idproducto, Remision.fecha)
     
 
     fecha_desde = datetime.strptime(fecha_desde, '%Y-%m-%d') if fecha_desde else None
@@ -198,6 +198,74 @@ def mostrar_compras(request: Request, db: Session = Depends(get_database_session
 @router.get("/depositos")
 async def mostrar_parametros_depositos(request: Request, db: Session = Depends(get_database_session), superusuario = Depends(auth.verificar_si_usuario_es_superusuario), usuario_actual: us.Usuario = Depends(auth.get_usuario_actual)):
     # proveedores = await routerproveedores.listado_proveedores(request=request, db=db)
-    productos = await routerproductos.listado_productos(request=request, db=db)
     depositos= await routerdepositos.listado_depositos(request=request, db=db)
-    return templates.TemplateResponse("/reportes/depositos.html", {"request": request, "Productos": productos, "Depositos":depositos, "datatables": True, "usuario_actual": usuario_actual})
+    productos = await routerproductos.listado_productos(request=request, db=db)
+    return templates.TemplateResponse("/reportes/depositos.html", {"request": request, "Depositos":depositos, "Productos": productos, "datatables": True, "usuario_actual": usuario_actual})
+
+@router.post("/depositos")
+def mostrar_depositos(request: Request, db: Session = Depends(get_database_session), usuario_actual: us.Usuario = Depends(auth.get_usuario_actual), fecha_desde = Body(), fecha_hasta = Body(), deposito = Body(), producto = Body(), tipo_movimiento = Body()):    
+    # join con gastos:unir con contrato en el join
+
+    
+    query_compras = db.query(
+        Producto.descripcion.label('descripcion'),
+        literal('Compra').label('tipo_movimiento'),
+        Factura_compra_detalle.idproducto.label('idproducto'),
+        func.sum(Factura_compra_detalle.cantidad).label('cantidad'),
+        Factura_compra_cabecera.fecha.label('fecha'),
+        Factura_compra_cabecera.deposito.label('deposito')
+    ).join(Factura_compra_cabecera, Factura_compra_detalle.idcabecera_compra == Factura_compra_cabecera.idfactura_compra
+    ).join(Producto, Factura_compra_detalle.idproducto == Producto.idproducto
+    ).group_by(Factura_compra_detalle.idproducto, Factura_compra_cabecera.fecha)
+
+    query_ventas = db.query(
+        Producto.descripcion.label('descripcion'),
+        literal('Venta').label('tipo_movimiento'),
+        Factura_venta_detalle.idproducto.label('idproducto'),
+        -(func.sum(Factura_venta_detalle.cantidad)).label('cantidad'),
+        Factura_venta_cabecera.fecha.label('fecha'),
+        Factura_venta_cabecera.deposito.label('deposito')
+    ).join(Factura_venta_cabecera, Factura_venta_detalle.idcabecera_venta == Factura_venta_cabecera.idfactura_venta
+    ).join(Producto, Factura_venta_detalle.idproducto == Producto.idproducto
+    ).group_by(Factura_venta_detalle.idproducto, Factura_venta_cabecera.fecha)
+
+    
+    query_remisiones = db.query(
+        literal('Remisión').label('tipo_movimiento'),
+        Remision.idproducto,
+        func.sum(Remision.cantidad).label('cantidad'),
+        Remision.fecha.label('fecha')
+    ).group_by(Remision.idproducto, Remision.fecha)
+
+    fecha_desde = datetime.strptime(fecha_desde, '%Y-%m-%d') if fecha_desde else None
+    fecha_hasta = datetime.strptime(fecha_hasta, '%Y-%m-%d') if fecha_hasta else None
+
+    # combinar las consultas con union all
+    subconsulta = union_all(query_compras, query_ventas).alias('subconsulta')
+
+    # filtrar según parámetros
+    consulta_final = db.query(
+        subconsulta.c.descripcion,
+        subconsulta.c.tipo_movimiento,
+        subconsulta.c.idproducto,
+        subconsulta.c.cantidad,
+        subconsulta.c.fecha
+    ).filter(
+        and_(
+            or_(func.date(subconsulta.c.fecha) >= fecha_desde if fecha_desde else True), # se convierte en true = 1 si fecha_desde es None (else True), o sea, no compara; se parsea como date para que solo tome la fecha, no la hora
+            or_(func.date(subconsulta.c.fecha) <= fecha_hasta if fecha_hasta else True), # se convierte en true = 1 si fecha_hasta es None (else True), o sea, no compara; se parsea como date para que solo tome la fecha, no la hora
+            or_(subconsulta.c.idproducto == producto if producto else True),
+            or_(
+                not tipo_movimiento, # para que devuelva "True" en caso de que tipo_movimiento sea nulo
+                tipo_movimiento == 'C' and subconsulta.c.tipo_movimiento == 'Compra',
+                tipo_movimiento == 'V' and subconsulta.c.tipo_movimiento == 'Venta',
+                tipo_movimiento == 'R' and subconsulta.c.tipo_movimiento == 'Remisión',
+            )
+        )
+    )
+
+    # Ejecutar la consulta combinada
+    resultado = consulta_final.all()
+    respuesta = [reporte.ReporteStockBase.from_orm(fila) for fila in resultado] # convierte los valores en una lista
+    #print(jsonable_encoder(respuesta))
+    return JSONResponse(jsonable_encoder(respuesta))
